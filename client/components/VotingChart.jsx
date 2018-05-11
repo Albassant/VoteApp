fdbe1b83d7921d6029d386af67f390808772c2be
Valentina <albassant@gmail.com>
2018-05-11 16:04:17 +0000

    custom legend

--- client/components/VotingChart.jsx
+++ client/components/VotingChart.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Bar } from 'react-chartjs-2';
+import { Bar, defaults } from 'react-chartjs-2';
+import { Parser as HtmlToReactParser } from 'html-to-react';
 
 import { withStyles } from 'material-ui/styles';
 import Card from 'material-ui/Card';
@@ -10,13 +11,18 @@ import { common } from './styles/commonStyles';
 import distinctColors from 'distinct-colors';
 import chroma from 'chroma-js';
 
+const htmlToReactParser = new HtmlToReactParser();
+defaults.global.animations = false;
+
 
 const styles = theme => ({
   container: {
     width: '75%',
+    height: '100%',
     margin: '0 auto',
     textAlign: 'center',
     padding: '40px',
+    overflow: 'auto',
     [theme.breakpoints.down('sm')]: {
       padding: '40px 0px',
       width: '90%',
@@ -35,69 +41,112 @@ const styles = theme => ({
   }
 });
 
-
-function VotingChart ({ datalabels, data, classes }) {
-
-  const palette = distinctColors({count: data.length});
-  const fillColors = palette.map(c => `rgba(${c.alpha(0.25).rgba()})`);
-  const outlineColors = palette.map(c => `rgba(${c.rgba()})`);
-  const datasets = data.map((d, i) => {
-      return {
-        label: datalabels[i],
-        data: [d],
-        backgroundColor: fillColors[i],
-        borderColor: outlineColors[i],
-        borderWidth: 1
+const options = {
+  maintainAspectRatio: true,
+  responsive: true,
+  title: {
+    display: false,
+  },
+  legend: {
+    display: false,
+  },
+  legendCallback: chart => {
+    var text = [];
+    text.push('<ul class="legend">');
+    for (var i = 0; i < chart.data.datasets.length; i++) {
+      text.push('<li><div>');
+      text.push(`<div class="rect" style="background-color:${chart.data.datasets[i].backgroundColor}"></div>`);
+      text.push(`<span>${chart.data.datasets[i].label}</span>`);
+      text.push('</li></div>');
+    }
+    text.push('</ul>');
+    console.log(text.join(''));
+    return text.join('');
+  },
+  scales: {
+    xAxes: [{
+      ticks: {
+        display: true,
+        autoSkip: false,
       }
-    });
+    }],
+    yAxes: [{
+      ticks: {
+        beginAtZero: true,
+        min: 0,
+        max: 100,
+        stepSize: 10,
+        callback: (value, index, values) => `${value}%`
+      }
+    }]
+  },
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem, data) {
+          var label = data.datasets[tooltipItem.datasetIndex].label || '';
 
-    console.log(datasets);
-
-  const chartData = {
-    labels: ['Votes'],
-    datasets: datasets
-  };
-
-  const options = {
-    maintainAspectRatio: true,
-    responsive: true,
-    title: {
-      display: false,
-    },
-    legend: {
-      display: true,
-      position: 'bottom',
-    },
-    scales: {
-      xAxes: [{
-        ticks: {
-          display: true,
-          autoSkip: false,
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          min: 0,
-          callback: function(value, index, values) {
-            if (Math.floor(value) === value) {
-              return value;
-            }
+          if (label) {
+              label += ': ';
           }
-        }
-      }]
-    },
-  };
+          label += tooltipItem.yLabel + '%';
+          return label;
+      },
+    }
+  }
+};
 
-  return (
-    <Card className={classes.container}>
-      <div className={classes.content}>
-        <Bar data={chartData}
-          options={options}
-        />
-      </div>
-    </Card>
-  );
+class VotingChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this._setup();
+
+    this.chart = null;
+    this.setChartRef = element => {
+      this.chart = element;
+      this.forceUpdate();
+    }
+  }
+
+  _setup(props) {
+    const { datalabels, data} = this.props;
+
+    const palette = distinctColors({count: data.length});
+    const fillColors = palette.map(c => `rgba(${c.alpha(0.25).rgba()})`);
+    const outlineColors = palette.map(c => `rgba(${c.rgba()})`);
+
+    const total = data.reduce((prev, curr) => prev + curr);
+
+    const datasets = data.map((d, i) => {
+        return {
+          label: datalabels[i],
+          data: [Math.round(d / total * 100)],
+          backgroundColor: fillColors[i],
+          borderColor: outlineColors[i],
+          borderWidth: 1
+        }
+      });
+
+    this.chartData = {
+      labels: ['Votes'],
+      datasets: datasets
+    };
+  }
+
+  render() {
+    const classes = this.props.classes;
+    return (
+      <Card className={classes.container}>
+        <div className={classes.content}>
+          <Bar data={this.chartData}
+            options={options}
+            ref={this.setChartRef}
+          />
+          { this.chart && htmlToReactParser.parse(this.chart.chartInstance.generateLegend()) }
+        </div>
+      </Card>
+    );
+  }
 }
 
 VotingChart.propTypes = {
